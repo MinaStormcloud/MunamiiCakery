8ca6af855eeef08f260d9145400c31e9576f2744
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-05-23 17:31:25 +0200

    Add project files.

---
+++ MunamiiCakery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MunamiiCakery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
